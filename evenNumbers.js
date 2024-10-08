5da3c04a463f1374436ef9fb211f5327073c7fe4
afquidangen <alyssafayequidangen@gmail.com>
2024-10-08 22:50:06 +0800

    For Loop Activity

--- evenNumbers.js
+++ evenNumbers.js
@@ -1,3 +1,12 @@
-for (let i = 0; i < 11; i++) {
-    console.log(i);
+for (let i = 1; i < 11; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
 }
+
+// //output:
+// 2
+// 4
+// 6
+// 8
+// 10
